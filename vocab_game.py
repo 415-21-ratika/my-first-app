2b968c44e82d5301f6469ccd8cefad449f6c0558
415-21-ratika <56312@yupparaj.ac.th>
2026-09-10 21:06:27 +0700

    vocab_game.py

--- vocab_game.py
+++ vocab_game.py
@@ -31,7 +31,74 @@ def show_result_dialog(ans1, ans2):
 
 # ตรวจข้อ 1
 if u_ans1 == "apple":
-    st.success("| ข้อ 1: ถูกต้อง")
+    st.success("✅ ข้อ 1: ถูกต้อง")
     score += 1
 else:
-    st.error(f"X ข้อ 1: ยังไม่ถูกต้อง (คุณตอบ '{u_ans1}')")
+    st.error(f"❌ ข้อ 1: ยังไม่ถูกต้อง (คุณตอบ '{u_ans1}')")
+
+# ตรวจข้อ 2
+if u_ans2 == "fish":
+   st.success("✅ ข้อ 2: ถูกต้อง")
+   score += 1
+else:
+    st.error(f"X ข้อ 2: ยังไม่ถูกต้อง (คุณตอบ '{u_ans2}')")
+
+# ✏️ [พื้นที่สำหรับนักเรียน] : เพิ่มตรวจข้อ 3, 4 ตรงนี้
+
+st.info(f"🏆 ได้คะแนนรวม: {score} คะแนน")
+
+if score == 2:
+    st.success("🎉 You win!")
+else:
+    st.error(" You lose!")
+
+
+# -------------------------------------------------
+# 1. ปุ่มเริ่มเล่นเกม
+# -------------------------------------------------
+st.button("🎮 เริ่มเล่นเกม", on_click=reset_game)
+
+# 2. แถบแสดงเวลานับถอยหลัง
+if "start" in st.session_state and not st.session_state.get("is_ended", False):
+    time_left = int(30 - (time.time() - st.session_state.start))
+
+if time_left > 0:
+    st.error(f"⏳ เหลือเวลา: {time_left} วินาที")
+else:
+    st.session_state.is_ended = True
+    st.rerun()
+
+st.divider()
+
+# 3. ช่องรับคำตอบ (ใช้ value ผูกกับตัวแปรตรงๆ เพื่อสั่งเคลียร์ได้)
+ans1 = st.text_input(
+    "ข้อ 1: An`a _ _ le` a day keeps the doctor away. 🍎",
+    value=st.session_state.ans1_val,
+)
+ans2 = st.text_input(
+    "ข้อ 2: Cats love to eat `f _ s h`. 🐟",
+    value=st.session_state.ans2_val,
+)
+
+# อัปเดตค่าล่าสุดเข้าตัวแปร
+st.session_state.ans1_val = ans1
+st.session_state.ans2_val = ans2
+
+# ✏️ [พื้นที่สำหรับนักเรียน] : เพิ่มข้อ 3, 4 ตรงนี้
+
+
+# 4. ปุ่มส่งคำตอบ
+if "start" in st.session_state and not st.session_state.get("is_ended", False):
+    if st.button("📥 ส่งคำตอบ") :
+        st.session_state.is_ended = True
+        st.rerun()
+
+    time.sleep(1)
+    st.rerun()
+
+# 5. แสดง Dialog ผลลัพธ์
+if st.session_state.get("is_ended", False):
+    show_result_dialog(ans1, ans2)
+
+st.divider()
+st.write("นางสาวรติกา กันทา เลขที่ 21 ม.4/15")
